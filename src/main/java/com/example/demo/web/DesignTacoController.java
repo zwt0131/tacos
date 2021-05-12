f47414dd545e77a8d4113de28f2c303e14556356
zhuwentao <1193034912@qq.com>
2021-05-12 20:51:48 +0800

    add data and modify the datasource of DesignTacoController.java

--- src/main/java/com/example/demo/web/DesignTacoController.java
+++ src/main/java/com/example/demo/web/DesignTacoController.java
@@ -2,7 +2,9 @@ package com.example.demo.web;
 
 import com.example.demo.Ingredient;
 import com.example.demo.Taco;
+import com.example.demo.data.IngredientRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,28 +28,27 @@ import javax.validation.Valid;
 @RequestMapping("/design")
 public class DesignTacoController {
 
+    private final IngredientRepository ingredientRepo;
+
+    @Autowired
+    public DesignTacoController(IngredientRepository ingredientRepo) {
+        this.ingredientRepo = ingredientRepo;
+    }
+
     @GetMapping
     public String showDesignForm(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-                new Ingredient("JACK", "Monterrey", Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
+        List<Ingredient> ingredients = new ArrayList<>();
+
+        ingredientRepo.findAll().forEach(ingredients::add);
         
-        Type[] types = Type.values();
+        Type[] types = Ingredient.Type.values();
         for (Type type : types) {
             // model{配料类型：子配料列表}
             model.addAttribute(type.toString().toLowerCase(),
                     filterByType(ingredients, type));
         }
 
+        // 这句话书中竟然删了。。。
         model.addAttribute("design", new Taco());
 
         return "design";
